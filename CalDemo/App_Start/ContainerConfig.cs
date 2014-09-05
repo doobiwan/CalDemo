e9cc3bcc53c9784e7c5b66c81cef2d910152e2e8
Craig Deubler <cdeubler@deconetwork.com>
2014-09-06 00:14:57 +1000

    Project & DI Skeleton

---
+++ CalDemo/App_Start/ContainerConfig.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace CalDemo
+{
+    public class ContainerConfig
+    {
+        public static void Configure(Action<System.Web.Http.Dependencies.IDependencyResolver> resolveWebApi, Action<System.Web.Mvc.IDependencyResolver> resolveMvc)
+        {
+            var builder = new ContainerBuilder();            
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+
+            resolveWebApi(new AutofacWebApiDependencyResolver(container));
+            resolveMvc(new AutofacDependencyResolver(container));
+        }
+    }
+}
